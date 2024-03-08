06d10533d83cec6bc7b637703bab85d9ca0df613
shedrack <aigbeshedrack0@gmail.com>
2024-03-08 05:58:33 -0800

    Update LoginScreen.js

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -24,7 +24,7 @@ const Login = ({ navigation }) => {
 
   const submit = async () => {
     setLoading(true);
-    navigation.navigate("Main");
+    // navigation.navigate("Main");
     try {
       const response = await axios.post(`${env.API_URL}/auth/login`, loginData);
       if (response.data) {
